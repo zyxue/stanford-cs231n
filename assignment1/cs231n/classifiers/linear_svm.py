fixed gradient calculation in svm_loss_naive

assignment1/cs231n/classifiers/linear_svm.py
@@ -36,11 +36,13 @@ def svm_loss_naive(W, X, y, reg):
         loss += margin
 
         # http://cs231n.github.io/optimization-1/
-        dW[:, j] += margin * X[i]
+        dW[:,j] += X[i]
+        dW[:,y[i]] -= X[i]
 
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
   loss /= num_train
+  dW /= num_train
 
   # Add regularization to the loss.
   loss += 0.5 * reg * np.sum(W * W)
@@ -53,6 +55,8 @@ def svm_loss_naive(W, X, y, reg):
   # loss is being computed. As a result you may need to modify some of the    #
   # code above to compute the gradient.                                       #
   #############################################################################
+  dW += reg * W
+
   return loss, dW
 
 