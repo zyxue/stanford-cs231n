32e9139649e393fa4ab281c6eb771b1f15d89ac4
Zhuyi Xue <alfred532008@gmail.com>
2016-06-05 15:26:26 +0000

    implemented rmsprop & adam

--- assignment2/cs231n/optim.py
+++ assignment2/cs231n/optim.py
@@ -100,7 +100,10 @@ def rmsprop(x, dx, config=None):
   # in the next_x variable. Don't forget to update cache value stored in      #  
   # config['cache'].                                                          #
   #############################################################################
-  pass
+  cache = (config['decay_rate'] * config['cache'] +
+           (1 - config['decay_rate']) * dx ** 2)
+  next_x = x - config['learning_rate'] * dx / (np.sqrt(cache) + config['epsilon'])
+  config['cache'] = cache
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
@@ -137,7 +140,13 @@ def adam(x, dx, config=None):
   # the next_x variable. Don't forget to update the m, v, and t variables     #
   # stored in config.                                                         #
   #############################################################################
-  pass
+  m = config['beta1'] * config['m'] + (1 - config['beta1']) * dx
+  v = config['beta2'] * config['v'] + (1 - config['beta2']) * (dx ** 2)
+  next_x = (x -
+            config['learning_rate'] * config['m']
+            / (np.sqrt(config['v']) + config['epsilon']))
+  config['m'] = m
+  config['v'] = v
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
